28a46c087c6415dacde1bb866a3d9ec07a462f21
Mohhit6075 <mohitsingh6266@gmail.com>
2024-09-11 01:03:42 +0530

    Create New1.jsx

---
+++ src/Components/Nested Components/New1/New1.jsx
@@ -0,0 +1,109 @@
+
+import { Slide } from "react-awesome-reveal";
+
+export const New1 = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="main-timeline after:block after:clear-both">
+            <Slide cascade damping={0.3} direction="left" triggerOnce>
+              <div className="timeline w-[50%] float-left m-[0_2px_0_0] max-md:m-[0_0_2px] max-md:w-full p-[3px_0]">
+                <a href="#" className="timeline-content text-[#ccc] bg-transparent text-left p-[20px_60px_20px_0] max-md:p-[20px_0_20px_120px] block relative">
+                    <div className="absolute bg-[#897be2b5] skew-y-[-20deg] w-10 top-0 bottom-0 -right-5 max-md:left-[30px]"></div>
+                  <div className="timeline-year text-[#444] text-[18px] text-center font-[600] leading-[100px] w-[100px] h-[100px] bg-[#897be2] translate-y-[-50%] top-[50%] right-[-50px] z-10 rounded-full absolute max-md:left-0 max-md:right-auto">
+                    <div className="absolute bg-white rounded-full shadow-[5px_5px_7px_rgba(0,0,0,0.4)] top-[10px] right-[10px] bottom-[10px] left-[10px] -z-10"></div>
+                    <div>12th Oct</div>
+                    </div>
+                  <h3 className="title text-3xl font-[500] text-[#7661ff] mb-1  max-md:scale-90 max-md:mb-0">Registration</h3>
+                  <p className="description text-[15px] text-left leading-normal font-normal tracking-wide max-md:scale-90">
+                    Version Beta is opening its doors for applications starting
+                    from August 20th until October 5th.To apply, all you need to
+                    do is to click on the “Register Here” Button above and fill
+                    the registration form. After the registration process is
+                    compeleted, A rundown of shortlisted candidates for the hack
+                    will be declared.
+                  </p>
+                </a>
+              </div>
+              </Slide>
+              <Slide cascade damping={0.3} direction="right" triggerOnce>
+              <div className="timeline w-[50%] float-right m-[0_2px_0_0] max-md:m-[0_0_2px] max-md:w-full p-[3px_0]">
+                <a href="#" className="timeline-content text-[#ccc] bg-transparent text-left p-[20px_0_20px_60px] max-md:p-[20px_0_20px_120px] block relative">
+                <div className="absolute bg-[#7661ffb5] skew-y-[-20deg] w-10 top-0 bottom-0 left-[-18px] right-auto max-md:left-[30px]"></div>
+                  <div className="timeline-year text-[#444] text-[18px] text-center font-[600] leading-[100px] w-[100px] h-[100px] bg-[#7661ff] translate-y-[-50%] top-[50%] left-[-50px] z-10 rounded-full absolute max-md:left-0 max-md:right-auto">
+                  <div className="absolute bg-white rounded-full shadow-[5px_5px_7px_rgba(0,0,0,0.4)] top-[10px] right-[10px] bottom-[10px] left-[10px] -z-10"></div>
+                  <div>13th Oct</div>
+                  </div>
+                  <h3 className="title text-3xl font-[500] text-[#7661ff] mb-1  max-md:scale-90 max-md:mb-0">Ideation</h3>
+                  <p className="description text-[15px] text-left leading-normal font-normal tracking-wide max-md:scale-90">
+                    Dive deep into current tech trends and challenges. Draw
+                    inspiration from industry leaders, innovative startups, and
+                    emerging technologies. Use brainstorming sessions to
+                    generate a plethora of ideas, no matter how wild or
+                    ambitious.{" "}
+                  </p>
+                </a>
+              </div>
+              </Slide>
+              <Slide cascade damping={0.3} direction="left" triggerOnce>
+              <div className="timeline w-[50%] float-left m-[0_2px_0_0] max-md:m-[0_0_2px] max-md:w-full p-[3px_0]">
+                <a href="#" className="timeline-content text-[#ccc] bg-transparent text-left p-[20px_60px_20px_0] max-md:p-[20px_0_20px_120px] block relative">
+                <div className="absolute bg-[#897be2b5] skew-y-[-20deg] w-10 top-0 bottom-0 -right-5 max-md:left-[30px]"></div>
+                  <div className="timeline-year text-[#444] text-[18px] text-center font-[600] leading-[100px] w-[100px] h-[100px] bg-[#897be2] translate-y-[-50%] top-[50%] right-[-50px] z-10 rounded-full absolute max-md:left-0 max-md:right-auto">
+                  <div className="absolute bg-white rounded-full shadow-[5px_5px_7px_rgba(0,0,0,0.4)] top-[10px] right-[10px] bottom-[10px] left-[10px] -z-10"></div>
+                  <div>14th Oct</div>
+                  </div>
+                  <h3 className="title text-3xl font-[500] text-[#7661ff] mb-1 max-md:scale-90 max-md:mb-0">Announcement</h3>
+                  <p className="description text-[15px] text-left leading-normal font-normal tracking-wide max-md:scale-90">
+                    After intense brainstorming and collaboration, Best ideas
+                    would be selected by judges, Their creativity and
+                    problem-solving skills have set the stage for the next
+                    challenge.{" "}
+                  </p>
+                </a>
+              </div>
+              </Slide>
+              <Slide cascade damping={0.3} direction="right" triggerOnce>
+              <div className="timeline w-[50%] float-right m-[0_2px_0_0] max-md:m-[0_0_2px] max-md:w-full p-[3px_0]">
+                <a href="#" className="timeline-content text-[#ccc] bg-transparent text-left p-[20px_0_20px_60px] max-md:p-[20px_0_20px_120px] block relative">
+                <div className="absolute bg-[#7661ffb5] skew-y-[-20deg] w-10 top-0 bottom-0 left-[-18px] right-auto max-md:left-[30px]"></div>
+                  <div className="timeline-year text-[#444] text-[18px] text-center font-[600] leading-[100px] w-[100px] h-[100px] bg-[#7661ff] translate-y-[-50%] top-[50%] left-[-50px] z-10 rounded-full absolute max-md:left-0 max-md:right-auto">
+                  <div className="absolute bg-white rounded-full shadow-[5px_5px_7px_rgba(0,0,0,0.4)] top-[10px] right-[10px] bottom-[10px] left-[10px] -z-10"></div>
+                  <div>15th Oct</div>
+                  </div>
+                  <h3 className="title text-3xl font-[500] text-[#7661ff] mb-1  max-md:scale-90 max-md:mb-0">Team</h3>
+                  <p className="description text-[15px] text-left leading-normal font-normal tracking-wide max-md:scale-90">
+                    Collaboration is key as students bring together their unique
+                    skills and expertise. Enlist your teams and prepare them to
+                    tackle the upcoming challenges. Get ready to synergize and
+                    innovate with your peers as the codathon journey continues!{" "}
+                  </p>
+                </a>
+              </div>
+              </Slide>
+              <Slide cascade damping={0.3} direction="left" triggerOnce>
+              <div className="timeline w-[50%] float-left m-[0_2px_0_0] max-md:m-[0_0_2px] max-md:w-full p-[3px_0]">
+                <a href="#" className="timeline-content text-[#ccc] bg-transparent text-left p-[20px_60px_20px_0] max-md:p-[20px_0_20px_120px] block relative">
+                <div className="absolute bg-[#897be2b5] skew-y-[-20deg] w-10 top-0 bottom-0 -right-5 max-md:left-[30px]"></div>
+                  <div className="timeline-year text-[#444] text-[18px] text-center font-[600] leading-[100px] w-[100px] h-[100px] bg-[#897be2] translate-y-[-50%] top-[50%] right-[-50px] z-10 rounded-full absolute max-md:left-0 max-md:right-auto">
+                  <div className="absolute bg-white rounded-full shadow-[5px_5px_7px_rgba(0,0,0,0.4)] top-[10px] right-[10px] bottom-[10px] left-[10px] -z-10"></div>
+                  <div>16th Oct</div>
+                  </div>
+                  <h3 className="title text-3xl font-[500] text-[#7661ff] mb-1  max-md:scale-90 max-md:mb-0">Hackathon</h3>
+                  <p className="description text-[15px] text-left leading-normal font-normal tracking-wide max-md:scale-90">
+                    Every truly novel concept appears insane at first, so come
+                    present yours. The time for brainstorming is past; now is
+                    the time to code and present hackathon presentations.{" "}
+                  </p>
+                </a>
+              </div>
+              </Slide>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
