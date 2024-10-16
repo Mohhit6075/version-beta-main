3c9bdd37294cd549098ba38fea6b4d12698414c9
Mohhit6075 <mohitsingh6266@gmail.com>
2024-10-16 20:34:40 +0530

    Update Swiper.jsx

--- src/Components/Nested Components/Swiper/Swiper.jsx
+++ src/Components/Nested Components/Swiper/Swiper.jsx
@@ -25,7 +25,7 @@ export default function App() {
           clickable: true,
           dynamicBullets: true,
         }}
-        navigation={true}
+        navigation={false}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
