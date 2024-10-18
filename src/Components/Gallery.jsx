116b11274f1d9e5a7f3ca2b14499203d3ffc82cd
Mohhit6075 <mohitsingh6266@gmail.com>
2024-10-18 19:28:33 +0530

    Update Gallery.jsx

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
       id="gallery"
       className="w-10/12 lg:w-7/12 xl:w-7/12 mx-auto mt-24 flex flex-col gap-16"
     >
-      <h1 className="text-[#897be2] text-center text-[45px] uppercase font-bold hover:scale-110 transition-transform duration-300">
+      <h1 className="text-[#897be2] text-center text-[45px] uppercase font-bold hover:scale-110 transition-transform duration-300  z-10">
         Gallery
       </h1>
       <Zoom cascade damping={0.3} triggerOnce>
