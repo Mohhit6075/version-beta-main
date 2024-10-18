9dcf850898b16c0d0c085d0ba27780f6c63cb164
Mohhit6075 <mohitsingh6266@gmail.com>
2024-10-18 19:28:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,11 +23,11 @@ const Navbar = () => {
 
   return (
     <>
-      <div id="navbar" className={ isSticky ? 'fixed w-full mx-auto flex justify-between items-center text-[#ddd] pt-4 bg-[#111] z-50 transition-all duration-200 md:justify-around lg:justify-around xl:justify-around' : 'static w-full mx-auto flex justify-around items-center text-[#ddd] pt-4'}>
-        <a href="https://www.istemanit.in/" target="blank">
+      <div id="navbar" className={ isSticky ? 'fixed w-full mx-auto flex justify-between items-center text-[#ddd] pt-4 bg-[#111] z-50 transition-all duration-200 md:justify-around lg:justify-around xl:justify-around' : 'static w-full mx-auto flex justify-around items-center text-[#ddd] pt-4 z-10'}>
+        <a href="https://www.istemanit.in/" target="blank" className=" z-10">
           <img src="/Images/Logo.svg" alt="logo" loading="lazy" />
         </a>
-        <div className="items-center hidden md:flex lg:flex xl:flex gap-2 md:gap-4 lg:gap-6 xl:gap-10 font-medium text-xl">
+        <div className="items-center hidden md:flex lg:flex xl:flex gap-2 md:gap-4 lg:gap-6 xl:gap-10 font-medium text-xl z-10">
           <a href="/" className="group">
             Home
             <div className="w-0 h-[1px] bg-[#6054aa] rounded-full group-hover:w-full transition-all duration-500"></div>
